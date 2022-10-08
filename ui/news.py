28748bb4515d49dd8d0bdfdc9b82fce257e1d65f
vladbakhteev <bakhteev_v@mail.ru>
2022-10-09 00:44:36 +0200

    add drop down menu

--- ui/news.py
+++ ui/news.py
@@ -11,20 +11,17 @@ role = st.radio("Роль", ROLE_MAPPING.keys())
 role_name = ROLE_MAPPING[role]
 
 topics = client.get_topics(role_name)
-tabs = st.tabs([topic['name'] for topic in topics])
 
-for tab, topic in zip(tabs, topics):
-    topic_id = topic['topic_id']
+topic_name = st.selectbox('Выбрать тему', [topic['name'] for topic in topics])
 
-    with tab:
-        frequency = client.get_trend(topic_id=topic_id)
-        ts_data = pd.DataFrame(frequency)
+topic_id = [t for t in topics if t['name'] == topic_name][0]['topic_id']
 
-        st.header('Встречаемость темы')
-        st.area_chart(data=ts_data, x='month', y='count')
+frequency = client.get_trend(topic_id=topic_id)
+ts_data = pd.DataFrame(frequency)
 
+st.header('Встречаемость темы')
+st.area_chart(data=ts_data, x='month', y='count')
 
-        publications = client.get_publications(topic_id=topic_id, num=SHOW_NUM_NEWS)
-
-        for html in feed(publications):
-            st.write(html, unsafe_allow_html=True)
+publications = client.get_publications(topic_id=topic_id, num=SHOW_NUM_NEWS)
+for html in feed(publications):
+    st.write(html, unsafe_allow_html=True)
